5cdbd4849a8f3916b0b3120c6ff5a23323d46538
dp.bobrova <dp.bobrova@tensor.ru>
2019-08-30 18:40:55 +0300

    lesson 2

--- src/App.js
+++ src/App.js
@@ -2,28 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+import Container from 'react-bootstrap/Container';
 import styled from 'styled-components';
+
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 function App() {
   return (
-    <div className="wrapper">
-          <SayFullName name="Diana" surname="Bobrova" link="vk.com"/>
-          <SayFullName name="Anna" surname="Ivanova" link="vk.com"/>
-          <SayFullName name="Ivan" surname="Petrov" link="vk.com"/>
-      </div>
+    <div className="App">
+      <HeaderWrapper>
+        <Container>
+          <Header />
+        </Container>
+      </HeaderWrapper>
+
+      <MenuWrapper>
+        <Container>
+          <Menu />
+        </Container>
+      </MenuWrapper>
+
+      <MainWrapper>
+        <Container>
+          <Main />
+        </Container>
+      </MainWrapper>
+    </div>
   );
 }
-
-function SayFullName(props) {
-  return (
-      <div>
-          <h1> Моё имя {props.name}, фамилия - {props.surname}</h1>
-          <a href={props.link}>Ссылка на мой профиль</a>
-      </div>
-  )
-}
-
 export default App;
